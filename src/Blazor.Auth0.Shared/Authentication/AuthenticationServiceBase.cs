44f81dfc0f41617ff0ad64154e5ad0352a8066fa
Dwayne Vogler <dwayne@dwaynevogler.com>
2019-07-24 21:47:30 -0600

    Added missing ampersand in url building code. fix for primary issue in https://github.com/Pegazux/Blazor.Auth0/issues/12

--- src/Blazor.Auth0.Shared/Authentication/AuthenticationServiceBase.cs
+++ src/Blazor.Auth0.Shared/Authentication/AuthenticationServiceBase.cs
@@ -111,7 +111,7 @@ namespace Blazor.Auth0.Shared.Authentication
             return $"https://{clientSettings.Auth0Domain}/authorize?" +
                       $"&response_type={responseType}" +
                       "&code_challenge_method=S256" +
-                      $"code_challenge={latestAuthorizeUrlCodeChallenge}" +
+                      $"&code_challenge={latestAuthorizeUrlCodeChallenge}" +
                       $"&state={latestAuthorizeUrlState}" +
                       $"&nonce={nonce}" +
                       $"&client_id={clientSettings.Auth0ClientId}" +
